70444e4f3efbedeb396c51ef3b2fc0db999c2aa7
stepbro72 <mridul.proses@gmail.com>
2024-03-05 23:52:17 +0530

    Add manifest.json and nut.js files for context menu functionality

---
+++ nut.js
@@ -0,0 +1,45 @@
+chrome.contextMenus.onClicked.addListener(genericOnClick);
+
+// A generic onclick callback function.
+function genericOnClick(info) {
+  selection = info.selectionText;
+  let targetSite;
+
+  switch (info.menuItemId) {
+    case 'nhentai':
+      targetSite = "https://nhentai.net/search/?q=";
+      break;
+    case 'sextb':
+      targetSite = "https://sextb.net/search/";
+      break;
+    case 'missav':
+      targetSite = "https://missav.com/en/search/";
+      break;
+  }
+
+  //open the selection in a new tab
+  chrome.tabs.create({ url: targetSite + selection, active: false });
+}
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    title: 'Open %s in nhentai.net',
+    type: 'normal',
+    id: 'nhentai',
+    contexts: ['selection']
+  });
+
+  chrome.contextMenus.create({
+    title: 'Open %s in sextb.net',
+    type: 'normal',
+    id: 'sextb',
+    contexts: ['selection']
+  });
+
+  chrome.contextMenus.create({
+    title: 'Open %s in missav.com',
+    type: 'normal',
+    id: 'missav',
+    contexts: ['selection']
+  });
+
+});
